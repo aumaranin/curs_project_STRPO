9e201fe3eb2cfeb25906d1ce2f0b3f7f63b1623c
aumaranin <aumaranin@gmail.com>
2022-10-17 18:06:14 +0300

    Добавлен проект StorehouseMS.

---
+++ StorehouseMS/src/main/java/ru/bmstu/curs_project_strpo/storehousems/StorehouseMsApplication.java
@@ -0,0 +1,15 @@
+package ru.bmstu.curs_project_strpo.storehousems;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StorehouseMsApplication
+{
+
+    public static void main(String[] args)
+    {
+        SpringApplication.run(StorehouseMsApplication.class, args);
+    }
+
+}
---
+++ StorehouseMS/src/test/java/ru/bmstu/curs_project_strpo/storehousems/StorehouseMsApplicationTests.java
@@ -0,0 +1,15 @@
+package ru.bmstu.curs_project_strpo.storehousems;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StorehouseMsApplicationTests
+{
+
+    @Test
+    void contextLoads()
+    {
+    }
+
+}
