c19421b063822656397ec31568c0475053d1f921
Маранин Александр <aumaranin@gmail.com>
2023-02-15 03:55:51 +0300

    Добавлены скрипты для запуска.

--- client2/src/main/java/ru/bmstu/curs_project_strpo/client2/properties/SetPropertis.java
+++ client2/src/main/java/ru/bmstu/curs_project_strpo/client2/properties/SetPropertis.java
@@ -2,6 +2,18 @@ package ru.bmstu.curs_project_strpo.client2.properties;
 
 public class SetPropertis
 {
+    public static Properties hybrid()
+    {
+        Properties properties = new Properties();
+        properties.setApigatewayURL("http://localhost:8080/");
+        properties.setStorehouseMsURL("http://storehousems:8040/");
+        properties.setCustomerMsURL("http://customerms:8041/");
+        properties.setPurchaseMsURL("http://purchasems:8042/");
+        properties.setCustomerBdURL("jdbc:postgresql://customerbd:5432/customerbd");
+        properties.setStorehouseBdURL("jdbc:postgresql://storehousebd:5432/storehousebd");
+        properties.setPurchaseBdURL("jdbc:postgresql://purchasebd:5432/purchasebd");
+        return properties;
+    }
     public static Properties docker()
     {
         Properties properties = new Properties();
--- start.sh
+++ start.sh
@@ -1,6 +1,12 @@
 #bash ./build_jar.sh
 
-java -jar ./ApiGateway/target/apigateway.jar &
-java -jar ./CustomerMS/target/customerms.jar &
-java -jar ./PurchaseMS/target/purchasems.jar &
-java -jar ./StorehouseMS/target/storehousems.jar &
+#java -jar ./ApiGateway/target/apigateway.jar &
+#java -jar ./CustomerMS/target/customerms.jar &
+#java -jar ./PurchaseMS/target/purchasems.jar &
+#java -jar ./StorehouseMS/target/storehousems.jar &
+
+docker compose up --build -d
+
+cd ./client2
+mvn clean javafx:run 2>/dev/null
+
